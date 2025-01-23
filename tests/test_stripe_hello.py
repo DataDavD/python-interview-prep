5ce3cd649bb3e41e70485bc1750a57c78ebba491
David Dansby <39511285+DataDavD@users.noreply.github.com>
2025-01-22 23:27:13 -0800

    Testing logger and add some notes

--- tests/test_stripe_hello.py
+++ tests/test_stripe_hello.py
@@ -1,6 +1,12 @@
 from stripe.hello import hello
 
+import logging
+
 
 def test_hello():
+    # create logger
+    logging.basicConfig(level=logging.INFO)
+    log = logging.getLogger(__name__)
+    log.info("test logger")
     assert hello() == "Hello, World!"
     assert hello("David") == "Hello, David!"
